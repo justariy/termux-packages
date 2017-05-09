a30758097489be94b87b63e34336b8b5c3ee7628
Henrik Grimler <Henrik@Grimler.se>
2017-05-09 22:05:17 +0200

    Add libsigsegv, needed for clisp

---
+++ packages/libsigsegv/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libsigsegv/
+TERMUX_PKG_DESCRIPTION="Library for Handling Page Faults in User Mode."
+TERMUX_PKG_VERSION=2.11
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=dd7c2eb2ef6c47189406d562c1dc0f96f2fc808036834d596075d58377e37a18
+TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
