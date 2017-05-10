c4687744f95149b196c7a6223dbaaf44c7952289
Henrik Grimler <Henrik@Grimler.se>
2017-05-10 20:23:24 +0200

    Added non-building version of clisp.
    clisp-arm.patch found here: https://sourceforge.net/p/clisp/patches/33/.
    Still doesn't build, there are some assembly commands that clang doesn't
    like. gcc might be needed.

---
+++ packages/clisp/build.sh
@@ -0,0 +1,111 @@
+TERMUX_PKG_HOMEPAGE=http://clisp.org/
+TERMUX_PKG_DESCRIPTION="Common Lisp is a high-level, all-purpose programming language."
+TERMUX_PKG_VERSION=2.49
+TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
+TERMUX_PKG_DEPENDS="libsigsegv"
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/clisp/release/${TERMUX_PKG_VERSION}/clisp-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bd2f6252c681337c0b6aa949fae3f92d3202dee3998c98a88ec28c72b115e866
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--srcdir=$TERMUX_PKG_SRCDIR"
+
+termux_step_pre_configure() {
+	CFLAGS=" -DNO_GENERATIONAL_GC -mcpu=arm1136j-s -no-integrated-as"
+}
+
+termux_step_configure_autotools () {
+	if [ ! -e "$TERMUX_PKG_SRCDIR/configure" ]; then return; fi
+
+	DISABLE_STATIC="--disable-static"
+	if [ "$TERMUX_PKG_EXTRA_CONFIGURE_ARGS" != "${TERMUX_PKG_EXTRA_CONFIGURE_ARGS/--enable-static/}" ]; then
+		# Do not --disable-static if package explicitly enables it (e.g. gdb needs enable-static to build)
+		DISABLE_STATIC=""
+	fi
+
+	DISABLE_NLS="--disable-nls"
+	if [ "$TERMUX_PKG_EXTRA_CONFIGURE_ARGS" != "${TERMUX_PKG_EXTRA_CONFIGURE_ARGS/--enable-nls/}" ]; then
+		# Do not --disable-nls if package explicitly enables it (for gettext itself)
+		DISABLE_NLS=""
+	fi
+
+	ENABLE_SHARED="--enable-shared"
+	if [ "$TERMUX_PKG_EXTRA_CONFIGURE_ARGS" != "${TERMUX_PKG_EXTRA_CONFIGURE_ARGS/--disable-shared/}" ]; then
+		ENABLE_SHARED=""
+	fi
+	HOST_FLAG="--host=$TERMUX_HOST_PLATFORM"
+	if [ "$TERMUX_PKG_EXTRA_CONFIGURE_ARGS" != "${TERMUX_PKG_EXTRA_CONFIGURE_ARGS/--host=/}" ]; then
+		HOST_FLAG=""
+	fi
+	LIBEXEC_FLAG=""
+
+	# Some packages provides a $PKG-config script which some configure scripts pickup instead of pkg-config:
+	mkdir "$TERMUX_PKG_TMPDIR/config-scripts"
+	for f in $TERMUX_PREFIX/bin/*config; do
+		test -f "$f" && cp "$f" "$TERMUX_PKG_TMPDIR/config-scripts"
+	done
+	export PATH=$TERMUX_PKG_TMPDIR/config-scripts:$PATH
+
+	# Avoid gnulib wrapping of functions when cross compiling. See
+	# http://wiki.osdev.org/Cross-Porting_Software#Gnulib
+	# https://gitlab.com/sortix/sortix/wikis/Gnulib
+	# https://github.com/termux/termux-packages/issues/76
+	local AVOID_GNULIB=""
+	AVOID_GNULIB+=" ac_cv_func_malloc_0_nonnull=yes"
+	AVOID_GNULIB+=" ac_cv_func_realloc_0_nonnull=yes"
+	AVOID_GNULIB+=" am_cv_func_working_getline=yes"
+	AVOID_GNULIB+=" gl_cv_func_dup2_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_fcntl_f_dupfd_cloexec=yes"
+	AVOID_GNULIB+=" gl_cv_func_fcntl_f_dupfd_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_fnmatch_posix=yes"
+	AVOID_GNULIB+=" gl_cv_func_getcwd_abort_bug=no"
+	AVOID_GNULIB+=" gl_cv_func_getcwd_null=yes"
+	AVOID_GNULIB+=" gl_cv_func_getcwd_path_max=yes"
+	AVOID_GNULIB+=" gl_cv_func_getcwd_posix_signature=yes"
+	AVOID_GNULIB+=" gl_cv_func_gettimeofday_clobber=no"
+	AVOID_GNULIB+=" gl_cv_func_gettimeofday_posix_signature=yes"
+	AVOID_GNULIB+=" gl_cv_func_link_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_lstat_dereferences_slashed_symlink=yes"
+	AVOID_GNULIB+=" gl_cv_func_malloc_0_nonnull=yes"
+	AVOID_GNULIB+=" gl_cv_func_memchr_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_mkdir_trailing_dot_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_mkdir_trailing_slash_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_select_detects_ebadf=yes"
+	AVOID_GNULIB+=" gl_cv_func_snprintf_posix=yes"
+	AVOID_GNULIB+=" gl_cv_func_snprintf_retval_c99=yes"
+	AVOID_GNULIB+=" gl_cv_func_snprintf_truncation_c99=yes"
+	AVOID_GNULIB+=" gl_cv_func_stat_dir_slash=yes"
+	AVOID_GNULIB+=" gl_cv_func_stat_file_slash=yes"
+	AVOID_GNULIB+=" gl_cv_func_strerror_0_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_symlink_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_tzset_clobber=no"
+	AVOID_GNULIB+=" gl_cv_func_unlink_honors_slashes=yes"
+	AVOID_GNULIB+=" gl_cv_func_unlink_honors_slashes=yes"
+	AVOID_GNULIB+=" gl_cv_func_vsnprintf_posix=yes"
+	AVOID_GNULIB+=" gl_cv_func_vsnprintf_zerosize_c99=yes"
+	AVOID_GNULIB+=" gl_cv_func_wcwidth_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_working_getdelim=yes"
+	AVOID_GNULIB+=" gl_cv_func_working_mkstemp=yes"
+	AVOID_GNULIB+=" gl_cv_func_working_mktime=yes"
+	AVOID_GNULIB+=" gl_cv_func_working_strerror=yes"
+	AVOID_GNULIB+=" gl_cv_header_working_fcntl_h=yes"
+
+	# NOTE: We do not want to quote AVOID_GNULIB as we want word expansion.
+	env $AVOID_GNULIB "$TERMUX_PKG_SRCDIR/configure" \
+		--disable-dependency-tracking \
+		--prefix=$TERMUX_PREFIX \
+		--disable-rpath --disable-rpath-hack \
+		$HOST_FLAG \
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS \
+		$DISABLE_NLS \
+		$ENABLE_SHARED \
+		$DISABLE_STATIC \
+		$LIBEXEC_FLAG
+}
+
+termux_step_make() {
+	if ls ./*akefile &> /dev/null; then
+		if [ -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
+			make -j $TERMUX_MAKE_PROCESSES
+		else
+			make -j $TERMUX_MAKE_PROCESSES ${TERMUX_PKG_EXTRA_MAKE_ARGS}
+		fi
+	fi
+}
