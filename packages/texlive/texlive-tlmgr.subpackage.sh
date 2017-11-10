b18db8571bda27c8c86b3354f987a0252b98e78b
Henrik Grimler <Henrik@Grimler.se>
2017-11-10 13:55:55 +0100

    texlive: Update descriptions of subpackages and update texlive-fulls file list to correct one

---
+++ packages/texlive/texlive-tlmgr.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_DESCRIPTION="TeX Lives package manager"
+TERMUX_SUBPKG_CONFLICTS="texlive-full"
+TERMUX_SUBPKG_INCLUDE="share/texlive/tlpkg share/texlive/texmf-dist/scripts/texlive/tlmgr.pl"
