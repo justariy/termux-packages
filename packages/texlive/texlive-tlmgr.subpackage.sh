43a886d6aa40279d55dda48d49d2982a4511b6af
Henrik Grimler <Henrik@Grimler.se>
2017-11-10 21:31:13 +0100

    texlive: move tlmgr related patches to texlive-tlmgr

--- packages/texlive/texlive-tlmgr.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="TeX Lives package manager"
-TERMUX_SUBPKG_CONFLICTS="texlive-full"
-TERMUX_SUBPKG_INCLUDE="share/texlive/tlpkg share/texlive/texmf-dist/scripts/texlive/tlmgr.pl"
