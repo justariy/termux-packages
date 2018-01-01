c25b8d0384b37fe9ca0dc7130c24968120fed87e
Henrik Grimler <Henrik@Grimler.se>
2018-01-01 17:17:58 +0100

    texlive: split subpackages further to decrease .deb sizes

---
+++ packages/texlive/texlive-fonts-type1.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_DESCRIPTION="Font files for the latex packages of TeX Live. Contains texmf-dist/fonts/type1."
+TERMUX_SUBPKG_DEPENDS="texlive-full"
+TERMUX_SUBPKG_INCLUDE="share/texlive/texmf-dist/fonts/type1"
--- packages/texlive/texlive-latex-doc.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Documentation for the latex packages of TeX Live. Contains texmf-dist/doc/latex."
-TERMUX_SUBPKG_DEPENDS="texlive-full"
-TERMUX_SUBPKG_INCLUDE="share/texlive/texmf-dist/doc/latex"
