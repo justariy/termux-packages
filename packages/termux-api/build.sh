77c96785708bed36c5f3877fb6c9054ca8cb3b8d
Henrik Grimler <Henrik@Grimler.se>
2018-06-20 19:55:03 +0200

    termux-api: rebuild after update

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://termux.com/add-on-api.html
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_VERSION=0.32
-TERMUX_PKG_SHA256=9e2af4e519e78616278acb7fce0ce90ffe53ba1d7ba314bed1495b8aa98692dc
+TERMUX_PKG_SHA256=0f481995e8ac08a71cd7290e59a4b38232072ff1a4d2386df8b52e6b908067f5
 TERMUX_PKG_SRCURL=https://github.com/Grimler91/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
