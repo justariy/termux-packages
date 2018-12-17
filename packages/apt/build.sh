321e057b6a01ad445970ca77422cdfa6b0ab4735
Henrik Grimler <henrik@grimler.se>
2018-12-17 08:21:40 +0100

    apt: add apt-ftparchive and libapt-pkg subpackages

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_DEPENDS="libcurl, liblzma, dpkg, gpgv, libc++, termux-exec"
 TERMUX_PKG_VERSION=1.4.8
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_REVISION=8
 TERMUX_PKG_SHA256=767ad7d6efb64cde52faececb7d3c0bf49800b9fe06f3a5b0132ab4c01a5b8f8
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -13,6 +13,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSE_NLS=OFF
 -DWITH_DOC=OFF
 "
+
+# ubuntu uses instead $PREFIX/lib instead of $PREFIX/libexec to
+# "Work around bug in GNUInstallDirs" (from apt 1.4.8 CMakeLists.txt).
+# Archlinux uses $PREFIX/libexec though, so let's force libexec->lib to
+# get same build result on ubuntu and archlinux.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-DCMAKE_INSTALL_LIBEXECDIR=lib"
+
 TERMUX_PKG_ESSENTIAL=yes
 TERMUX_PKG_CONFFILES="etc/apt/sources.list etc/apt/trusted.gpg"
 TERMUX_PKG_CONFLICTS=apt-transport-https
@@ -22,7 +29,6 @@ bin/apt-cdrom
 bin/apt-extracttemplates
 bin/apt-sortpkgs
 etc/apt/apt.conf.d
-lib/apt/apt-helper
 lib/apt/methods/bzip2
 lib/apt/methods/cdrom
 lib/apt/methods/mirror
@@ -30,13 +36,11 @@ lib/apt/methods/rred
 lib/apt/planners/
 lib/apt/solvers/
 lib/dpkg/
-lib/libapt-inst.so
 "
 
 termux_step_post_make_install() {
 	printf "# The main termux repository:\ndeb https://termux.net stable main\n" > $TERMUX_PREFIX/etc/apt/sources.list
 	cp $TERMUX_PKG_BUILDER_DIR/trusted.gpg $TERMUX_PREFIX/etc/apt/
-	rm $TERMUX_PREFIX/include/apt-pkg -r
 
 	# apt-transport-tor
 	ln -sfr $TERMUX_PREFIX/lib/apt/methods/http $TERMUX_PREFIX/lib/apt/methods/tor
---
+++ packages/apt/libapt-pkg.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_DESCRIPTION="This package contains the header files and libraries for developing with APT's libapt-pkg Debian package manipulation library and the libapt-inst deb/tar/ar library"
+TERMUX_SUBPKG_INCLUDE="
+include/apt-pkg
+lib/libapt-inst.so
+"
